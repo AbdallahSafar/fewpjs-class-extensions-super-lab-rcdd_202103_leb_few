98cdd8699804591d451a8b1400ae805b7aa0ce36
Abdallah Safar <abdallah.safar01@gmail.com>
2021-05-14 22:42:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your classes here
+
+class Tree {
+  constructor( species ) {
+    this.species = species
+  }
+
+  static definition() {
+    return 'The tree is a long plant.'
+  }
+
+}
+
+class Deciduous extends Tree {
+  constructor( species, name ) {
+    super( species )
+    this.name = name
+  }
+
+  static definition() {
+    return `${super.definition()} Deciduous trees shed their leaves annually.`
+  }
+}
+
+class Evergreen extends Tree {
+  constructor( species, name ) {
+    super( species )
+    this.name = name
+  }
+
+  static definition() {
+    return `${super.definition()} Evergreens keep their leaves all year round.`
+  }
+}
